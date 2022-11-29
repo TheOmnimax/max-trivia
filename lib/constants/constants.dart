810133f98c6ce87fb92b238878469bfc070d1ebe
TheOmnimax <max@maxshaberman.com>
2022-11-29 06:38:45 -0500

    Set URL to server

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,8 +1,8 @@
 export 'enums.dart';
 export 'regex.dart';
 
-// const baseUrl = 'https://max-trivia.uc.r.appspot.com/'; // Real app
-const baseUrl = 'http://127.0.0.1:8000/'; // Web testing
+const baseUrl = 'https://max-trivia.uc.r.appspot.com/'; // Real app
+// const baseUrl = 'http://127.0.0.1:8000/'; // Web testing
 // const baseUrl = 'ws://10.0.2.2:8000/'; // Android testing
 
 const sendHeaders = {
